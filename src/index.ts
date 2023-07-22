b073ffa3a2c3526a9822b4a289cfbfded07289f6
Heloi Neto <hey@heloineto.com>
2023-07-21 23:46:50 -0300

    chore: change the way functions are declared

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import getApplication from './actions/getApplication';
 import getCurrentLanguage from './actions/getCurrentLanguage';
 import hasPermissions from './actions/hasPermissions';
 import heightChange from './actions/heightChange';
+import hideNavbar from './actions/hideNavbar';
 import segment from './actions/segment';
 import sendCommand from './actions/sendCommand';
 import showModal from './actions/showModal';
+import showNavbar from './actions/showNavbar';
 import startLoading from './actions/startLoading';
 import stopLoading from './actions/stopLoading';
 import toast from './actions/toast';
@@ -18,9 +20,11 @@ const iframe = {
   getCurrentLanguage,
   hasPermissions,
   heightChange,
+  hideNavbar,
   segment,
   sendCommand,
   showModal,
+  showNavbar,
   startLoading,
   stopLoading,
   toast,
@@ -37,9 +41,11 @@ export type * from './actions/getApplication';
 export type * from './actions/getCurrentLanguage';
 export type * from './actions/hasPermissions';
 export type * from './actions/heightChange';
+export type * from './actions/hideNavbar';
 export type * from './actions/segment';
 export type * from './actions/sendCommand';
 export type * from './actions/showModal';
+export type * from './actions/showNavbar';
 export type * from './actions/startLoading';
 export type * from './actions/stopLoading';
 export type * from './actions/toast';
