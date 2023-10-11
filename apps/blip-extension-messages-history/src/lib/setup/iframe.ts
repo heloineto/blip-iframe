ae9fdadd993edb8497ae65b6ba351da354f5406d
Heloi Neto <hey@heloineto.com>
2023-10-11 07:28:21 -0300

    feat: remove extension example

--- apps/blip-extension-messages-history/src/lib/setup/iframe.ts
+++
@@ -1,9 +0,0 @@
-import { IframeMessageProxy, blip } from 'blip-iframe';
-import track from 'lib/utils/track';
-
-IframeMessageProxy.listen();
-
-// The iframe's parent minimum height is 100%. By setting it's height to 0px,
-// the extension will take 100% of the remaining height.
-void blip.heightChange({ height: 0 });
-void track('opened');
